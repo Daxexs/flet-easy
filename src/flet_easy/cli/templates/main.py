cdbf4d1c6b332b4b829c3479b2c423cf1eec0d29
Daxexs <Dxsx1226dev@gmail.com>
2024-04-16 18:30:02 -0500

    use path views

--- src/flet_easy/cli/templates/main.py
+++ src/flet_easy/cli/templates/main.py
@@ -1,17 +1,14 @@
-import flet as ft
+from pathlib import Path
+
 import flet_easy as fs
 from core.config import ConfigApp
-from views import counter, index
 
-app = fs.FletEasy(route_init="/home")
+app = fs.FletEasy(
+    route_init="/home",
+    path_views=Path(__file__).parent / "views"
+    )
 
-app.add_pages(
-    [
-        index,
-        counter,
-    ]
-)
 ConfigApp(app)
 
 # We run the application
-app.run(view=ft.AppView.WEB_BROWSER)
+app.run()
--- src/flet_easy/cli/templates/views/__init__.py
+++
@@ -1,2 +0,0 @@
-from views.counter import counter
-from views.index import index
