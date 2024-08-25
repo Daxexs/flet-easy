75d11f4cb39e5017453ac650a3c5d3ef3f4af09d
Daxexs <Dxsx1226dev@gmail.com>
2024-08-25 12:43:27 -0500

    Updating cli (fs) with Cookiecutter

--- src/flet_easy/cli/main.py
+++ src/flet_easy/cli/main.py
@@ -1,12 +1,8 @@
 import argparse
 import contextlib
-from pathlib import Path
 
-from rich.prompt import Prompt
+from cookiecutter.main import cookiecutter
 from rich_argparse import RichHelpFormatter
-from tomlkit import dump
-
-from .copy import _copy_template
 
 VERSION = "0.2.2"
 
@@ -16,31 +12,12 @@ RichHelpFormatter.styles["argparse.prog"] = "blue"
 
 
 def init_app():
-    toml_dict = {"project": {}}
-    pwd = Path.cwd()
-
-    name = Prompt.ask("Projet Name", default=pwd.name)
-    version = Prompt.ask("Project Version", default="0.1.0")
-    license = Prompt.ask(
-        "Project License",
-        default="MIT",
+    cookiecutter(
+        template="gh:Daxexs/fs-template-dxs",
+        overwrite_if_exists=True,
+        checkout="main",
+        accept_hooks=True,
     )
-    author = Prompt.ask("Project Author")
-    email = Prompt.ask("Email Author")
-    manifest = Prompt.ask(
-        "Requires creation of manifest.json (Web-PWA)", choices=["n", "y"], default="n"
-    )
-
-    toml_dict["project"]["name"] = name
-    toml_dict["project"]["version"] = version
-    toml_dict["project"]["license"] = license
-    toml_dict["project"]["authors"] = [{"name": author, "email": email}]
-    toml_dict["project"]["dependencies"] = ["flet", "flet-easy"]
-
-    _copy_template("templates", pwd / name, manifest)
-
-    with open(f"{pwd}/{name}/pyproject.toml", "w") as file:
-        dump(toml_dict, file)
 
 
 def run():
