be33992d009d2b8e7be6c3edb2b561546cfc2459
Daxexs <Dxsx1226dev@gmail.com>
2024-10-10 11:27:30 -0500

    :wastebasket: deprecate: remover v0.2.4

--- 0.2.4/assets/js/dynamic-year.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var yearSpan = document.getElementById('dynamic-year');
-    if (yearSpan) {
-      yearSpan.textContent = new Date().getFullYear();
-    }
-  });
-  
