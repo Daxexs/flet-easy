4a4aefa7afe47e6f7bbdcfd4c392c27729fc157d
Daxexs <Dxsx1226dev@gmail.com>
2025-08-09 19:23:11 -0500

    ♻️ refactor: move page initialization to the end of __init__.py for better readability

--- src/flet_easy/__init__.py
+++ src/flet_easy/__init__.py
@@ -1,7 +1,7 @@
 from flet_easy.datasy import Datasy
 from flet_easy.extra import Redirect
 from flet_easy.extrasJwt import Algorithm, PemKey, SecretKey, encode_HS256, encode_RS256
-from flet_easy.fletEasy import FletEasy, page
+from flet_easy.fletEasy import FletEasy
 from flet_easy.inheritance import (
     Keyboardsy,
     Ref,
@@ -11,3 +11,5 @@ from flet_easy.inheritance import (
 )
 from flet_easy.jwt import EasyKey, decode, decode_async
 from flet_easy.pagesy import AddPagesy, Pagesy, MiddlewareRequest
+
+page = FletEasy.page
